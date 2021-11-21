36abaf75fe9a1ddc492a389d6a14f1f3ce56146c
KarlStarkweather <stark13@usa.net>
2021-11-21 13:54:21 -0800

    create AddressBook constructor and fullName method

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+
+// Business Logic for AddressBook --------------
+function AddressBook() {
+  this.contacts = {};
+}
+
+// Business Logic for Contacts --------------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
